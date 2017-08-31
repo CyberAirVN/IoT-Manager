200a862f02dd4ac85971f7aa256403a3548c21be
Mr DevNET <info@devnet.vn>
2017-08-31 22:00:57 +0700

    #2 change token

--- config/lib/token.js
+++ config/lib/token.js
@@ -1,31 +1,49 @@
 'use strict';
 
-const jwt = require('jsonwebtoken'),
-  fs = require('fs'),
-  path = require('path'),
-  privateKey = path.resolve('./config/certificate/private-key.pem'),
-  publicKey = path.resolve('./config/certificate/public-key.pem');
+// const jwt = require('jsonwebtoken'),
+//   fs = require('fs'),
+//   path = require('path'),
+//   privateKey = path.resolve('./config/certificate/private-key.pem'),
+//   publicKey = path.resolve('./config/certificate/public-key.pem');
+//
+// exports.encode = function (data) {
+//
+//   var token = jwt.sign(data, fs.readFileSync(privateKey), {
+//     algorithm: 'RS256'
+//   });
+//   return token;
+// };
+//
+// exports.decode = function (token, callback) {
+//   if (!token) {
+//     callback(null);
+//   } else {
+//     jwt.verify(token, fs.readFileSync(publicKey), {
+//       algorithms: ['RS256']
+//     }, function (err, data) {
+//       if (err) {
+//         callback(null);
+//       } else {
+//         callback(data);
+//       }
+//     });
+//   }
+// };
 
-exports.encode = function (data) {
+var crypto = require('crypto'),
+  algorithm = 'aes-256-ctr',
+  password = 'jsh9dUG52s18';
 
-  var token = jwt.sign(data, fs.readFileSync(privateKey), {
-    algorithm: 'RS256'
-  });
-  return token;
+exports.encode = function(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
 };
 
-exports.decode = function (token, callback) {
-  if (!token) {
-    callback(null);
-  } else {
-    jwt.verify(token, fs.readFileSync(publicKey), {
-      algorithms: ['RS256']
-    }, function (err, data) {
-      if (err) {
-        callback(null);
-      } else {
-        callback(data);
-      }
-    });
-  }
+exports.decode = function(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
 };
